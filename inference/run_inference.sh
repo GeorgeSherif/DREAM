8a1973a4eccf28364c303d5b5cff55a12301ad2f
GeorgeSherif <65810674+GeorgeSherif@users.noreply.github.com>
2024-04-26 20:30:29 +0300

    Update run_inference.sh

--- inference/run_inference.sh
+++ inference/run_inference.sh
@@ -1,5 +1,5 @@
 python prepare_files.py ../Unmodified_Data/MADAR.corpus.Cairo.tsv ../Data/MADAR.corpus.MSA.tsv
 
-python inference.py ./formatted_files/MSA_EGY.csv
+python inference.py ./formatted_files/MSA_EGY.csv bigscience/mt0-base
 
 python evaluation.py ./translated_zero_shot/inference_EGY_mt0-base.csv ALG
